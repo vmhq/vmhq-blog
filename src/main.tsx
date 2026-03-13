4329d430f3f7a89143ba9abaf4b40d75a9862efc
Vicente Méndez <viro.mng@gmail.com>
2026-03-13 20:12:55 -0300

    feat: add Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")!).render(
     <HelmetProvider>
       <App />
       <Analytics />
+      <SpeedInsights />
     </HelmetProvider>
   </React.StrictMode>
 );
